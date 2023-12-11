994d8adbdcad740db2f641e61c6d25a0dfab7eaf
Igor Shishov <i9core13900@gmail.com>
2023-12-11 13:53:51 +0300

    fix: translated comments, fix encoding

--- Hook/dllmain.cpp
+++ Hook/dllmain.cpp
@@ -27,32 +27,31 @@ void WritePacketBytesToFile(void* packet, size_t len, const std::string& filenam
 
         uintptr_t address = reinterpret_cast<uintptr_t>(packet);
         file << "PacketAddress: 0x" << std::hex << std::uppercase << address << std::endl;
-
         file.close();
     }
 }
 
-// Тип оригинальной функции
+// Original type func
 // typedef void (__stdcall OrigFuncType)(void* Size, size_t size);
 typedef int(__stdcall* pSendPacket) (void* Src, size_t Size);
 
-// Адрес оригинальной функции
+// Get game address in memory
 DWORD GameBase = (uintptr_t)GetModuleHandle(NULL);
 
 DWORD SendPacketAddress = GameBase + 0x367CE0;
 DWORD BaseAddress = GameBase + 0xA88F70;
 
-// Оригинальная функция
+// Original function
 pSendPacket oSendPacket = (pSendPacket)(SendPacketAddress);
 
 // unsigned char packetBytes[] = { 0x02, 0x00, 0xB0, 0x57, 0x0B, 0x00 };
 // void* packet = static_cast<void*>(packetBytes);
 // size_t len = sizeof(packetBytes);
 
-// Наша функция-перехватчик
+// Hooked function
 int __stdcall hSendPacket(void* packet, size_t len)
 {
-    // DEBUG Сохраняем наш пакет в файл.
+    // DEBUG Save packet's bytes to file
     WritePacketBytesToFile(packet, len, "output.txt");
 
     DWORD ecx_val = *(DWORD*)(*(DWORD*)BaseAddress + 0x20);
@@ -70,16 +69,16 @@ int __stdcall hSendPacket(void* packet, size_t len)
 /*
 extern "C" __declspec(dllexport) int __stdcall CallSendPacketFromExternal() {
     DWORD ecx_val = *(DWORD*)(*(DWORD*)BaseAddress + 0x20);
-    wprintf(L"Значение переменной ecx_val: %lu\n", BaseAddress);
+    wprintf(L"ecx_val: %lu\n", BaseAddress);
     return 1;
 }
 */
 
 unsigned int __stdcall mainThread(void* pArguments)
 {
-    // Инициализация Detours
+    // Init Detours
     DetourRestoreAfterWith();
-    // Перехватываем функцию
+    // Start hook game function
     DetourTransactionBegin();
     DetourUpdateThread(GetCurrentThread());
     DetourAttach(&(PVOID&)oSendPacket, hSendPacket);
@@ -97,7 +96,7 @@ BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReser
         hThread = (HANDLE)_beginthreadex(NULL, 0x1000, &mainThread, (void*)hModule, 0, &threadId);
         break;
     case DLL_PROCESS_DETACH:
-        // Отменяем перехват
+        // Cancel detouring
         DetourTransactionBegin();
         DetourUpdateThread(GetCurrentThread());
         DetourDetach(&(PVOID&)oSendPacket, hSendPacket);
